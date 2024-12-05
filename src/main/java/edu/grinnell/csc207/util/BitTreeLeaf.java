b36a152c936327db0ce211c1aadda52a93e7defc
HouckDev <houckdeveloper@gmail.com>
2024-12-05 15:39:49 -0600

    Implement basic bit tree classes

---
+++ src/main/java/edu/grinnell/csc207/util/BitTreeLeaf.java
@@ -0,0 +1,17 @@
+package edu.grinnell.csc207.util;
+
+public class BitTreeLeaf implements BitTreeNode {
+
+  @Override
+  public BitTreeNode getLeftChild() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getLeftChild'");
+  }
+
+  @Override
+  public BitTreeNode getRightChild() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getRightChild'");
+  }
+  
+}
---
+++ src/main/java/edu/grinnell/csc207/util/BitTreeNode.java
@@ -0,0 +1,5 @@
+package edu.grinnell.csc207.util;
+public interface BitTreeNode {
+  public BitTreeNode getLeftChild();
+  public BitTreeNode getRightChild();
+}
